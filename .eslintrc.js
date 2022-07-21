643d22d2eedc3fc5bba88102df9a3f147bc4ef9e
gardenjoon <wees114@gmail.com>
2022-07-21 14:03:06 +0900

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,3 @@
-// module.exports = {
-//     root: true,
-//     extends: "@react-native-community",
-//     parser: "@typescript-eslint/parser",
-//     plugins: ["@typescript-eslint"],
-//     overrides: [
-//         {
-//             files: ["*.ts", "*.tsx"],
-//             rules: {
-//                 "@typescript-eslint/no-shadow": ["error"],
-//                 "no-shadow": "off",
-//                 "no-undef": "off",
-//             },
-//         },
-//     ],
-//     rules: {
-//         "@typescript-eslint/interface-name-prefix": "on",
-//         "@typescript-eslint/explicit-function-return-type": "on",
-//         "@typescript-eslint/explicit-module-boundary-types": "on",
-//         "@typescript-eslint/no-explicit-any": "on",
-//     },
-//     extends: [
-//         "eslint:recommended",
-//         "plugin:react/recommended",
-//         "airbnb",
-//         "plugin:@typescript-eslint/recommended",
-//         "plugin:prettier/recommended",
-//     ],
-// };
-
 module.exports = {
     parser: "@typescript-eslint/parser",
     parserOptions: {
